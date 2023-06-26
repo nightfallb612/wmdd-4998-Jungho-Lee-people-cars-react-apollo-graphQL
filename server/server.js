06ac2d35a10d043695c654515a84dfbac8f1d635
Jungho Lee <nightfallb612@gmail.com>
2023-06-26 00:40:07 -0700

    Upload server code

---
+++ server/server.js
@@ -0,0 +1,38 @@
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { resolvers, typeDefs } from "./src/peopleCarsScheme";
+import bodyParser from "body-parser";
+
+const startApolloServer = async (resolvers, typeDefs) => {
+  const app = express();
+
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  app.use(
+    "/graphql",
+    cors(),
+    bodyParser.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    })
+  );
+
+  // Modified server startup
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
+};
+
+startApolloServer(resolvers, typeDefs);
